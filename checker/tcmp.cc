682b8c3a2ae7f3aea614d1826db58295e71eb8f4
test-pps-admin <test.pps.github@gmail.com>
2023-04-05 00:43:25 +0900

    Update: path 'checker/tcmp.cc' - Updated by 'admin'

--- checker/tcmp.cc
+++ checker/tcmp.cc
@@ -3,7 +3,11 @@
 using namespace std;
  
 int main(int argc, char** argv) {
-    puts("SDF");
+    puts(argv[0]);
+    puts(argv[1]);
+    puts(argv[2]);
+    puts(argv[3]);
+    
     setName("token compare");
     registerTestlibCmd(argc, argv);
  
