440adfcfa9a39befc6a398015dedcaaf023d1742
test-pps-admin <test.pps.github@gmail.com>
2023-04-05 00:54:00 +0900

    Update: path 'checker/tcmp.cc' - Updated by 'admin'

--- checker/tcmp.cc
+++ checker/tcmp.cc
@@ -1,12 +1,35 @@
 #include "testlib.h"
  
 using namespace std;
+
+char buf[101010];
  
 int main(int argc, char** argv) {
     puts(argv[0]);
     puts(argv[1]);
     puts(argv[2]);
     puts(argv[3]);
+
+    FILE* in = fopen(argv[1], "r");
+    
+    fscanf(in, "%s", buf);
+    printf("in: %s\n", buf);
+
+    fclose(in);
+
+    FILE* out = fopen(argv[2], "r");
+    
+    fscanf(out, "%s", buf);
+    printf("out: %s\n", buf);
+
+    fclose(out);
+
+    FILE* ans = fopen(argv[3], "r");
+    
+    fscanf(ans, "%s", buf);
+    printf("ans: %s\n", buf);
+
+    fclose(ans);
     
     setName("token compare");
     registerTestlibCmd(argc, argv);
